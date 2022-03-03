e41a8d9d808fabdc93b196e1016eb2989ba03e27
Anderson Adolfo <anderson.adolfo1998@gmail.com>
2022-03-02 21:09:20 -0300

    Separando o Servidor do Socket

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,5 @@
-import express from "express";
-import { createServer } from "http";
-import { Server, Socket } from "socket.io";
-import path from "path";
+import { server } from "./http";
+import "./websocket/ChatService";
 
-const app = express();
-
-const server = createServer(app);
-
-app.use(express.static(path.join(__dirname, "..", "public")))
-
-const io = new Server(server);
-
-io.on("connection", (socket) => {
-    console.log("Socket", socket.id);
-});
 
 server.listen(3000, () => console.log("server is running on port 3000!"));
